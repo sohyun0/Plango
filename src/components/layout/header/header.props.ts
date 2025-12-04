3c63d217360c7c312db5b1fd4adddf65810ce720
celine <wisohyun0@gmail.com>
2025-12-04 18:14:05 +0900

    feat: #195 메뉴명 변경

--- src/components/layout/header/header.props.ts
+++ src/components/layout/header/header.props.ts
@@ -5,7 +5,7 @@ type userOption = {
 
 export const userOptions: userOption[] = [
   { url: "/my/history", menuName: "마이 히스토리" },
-  { url: "/my", menuName: "계정관리" },
+  { url: "/my", menuName: "계정 설정" },
   { url: "/team/join", menuName: "팀참여하기" },
   { url: "/team/create", menuName: "팀생성하기" },
 ];
