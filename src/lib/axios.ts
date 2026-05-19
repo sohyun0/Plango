931e22957c7cb3dd5bf715f567471c853a071a75
celine <wisohyun0@gmail.com>
2026-05-20 00:29:42 +0900

    refactor: axios BFF 기본 경로

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,66 +1,18 @@
 import axios, { AxiosInstance } from "axios";
-import { isNoAuthAxios } from "./utils";
-
-import { getAccessToken, logoutDirect, requestNewAccessToken } from "./token";
 
 /**
- * axios 요청시 헤더에 토큰 추가 및 토큰 만료시 재발급
- * @author sohyun
+ * BFF로 요청을 전달하는 axios 인스턴스
+ * 클라이언트 환경에서는 /api로 호출하고, 서버 환경에서는 백엔드 URL을 사용합니다.
  */
+const isBrowser = typeof window !== "undefined";
 
-// axios 인스턴스
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: isBrowser ? "/api" : process.env.NEXT_PUBLIC_API_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
+
 export default axiosInstance;
-
-// 요청 인터셉터: axios 요청시 accessToken 자동 추가
-axiosInstance.interceptors.request.use(
-  config => {
-    // 토큰이 필요 없는 URL
-    if (isNoAuthAxios(config)) return config;
-
-    const accessToken = getAccessToken();
-    if (!accessToken) return config;
-
-    // 인증이 필요한 요청 처리
-    config.headers = config.headers ?? {};
-    config.headers.Authorization = `Bearer ${accessToken}`;
-    return config;
-  },
-  err => Promise.reject(err),
-);
-
-// 응답 인터셉터: axios 요청시 accessToken 만료시 재발급
-axiosInstance.interceptors.response.use(
-  res => res,
-  async err => {
-    const { response, config } = err;
-    if (!config || response?.status !== 401) {
-      return Promise.reject(err);
-    }
-
-    //  무한 루프 방지
-    if (config._retry) {
-      return Promise.reject(err);
-    }
-    config._retry = true;
-
-    // refresh route 요청해서 새 accessToken 받아오기
-    const newToken = await requestNewAccessToken();
-
-    // 기존 요청 재시도
-    if (newToken) {
-      config.headers.Authorization = `Bearer ${newToken}`;
-      return axiosInstance(config);
-    }
-
-    // refresh 실패시 로그아웃
-    await logoutDirect();
-    window.location.href = "/login";
-  },
-);
