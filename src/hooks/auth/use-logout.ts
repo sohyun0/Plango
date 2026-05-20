82dd7a8700058fc1944859cc9fcd215994b8f59b
celine <wisohyun0@gmail.com>
2026-05-20 17:20:28 +0900

    refactor: 로그아웃로직 mutation 으로 변경

--- src/hooks/auth/use-logout.ts
+++ src/hooks/auth/use-logout.ts
@@ -1,22 +1,32 @@
-import { authQueryKeys } from "@/queryKeys/Auth";
-import { useQueryClient } from "@tanstack/react-query";
+import axiosInstance from "@/lib/axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 /**
  * 로그아웃 처리 훅, 필요시 메인 페이지 리다이렉트
  * @author sohyun
  */
 
+const postLogout = async () => {
+  const res = await axiosInstance.post("/auth/logout");
+  return res.data;
+};
+
 const useLogout = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
   return async ({ isRedirect = true } = {}) => {
-    await fetch("/api/auth/logout", { method: "POST" });
+    return useMutation({
+      mutationFn: postLogout,
+      onSettled: () => {
+        queryClient.clear();
+        router.refresh();
 
-    queryClient.setQueryData(authQueryKeys.user, null);
-    queryClient.removeQueries({ queryKey: authQueryKeys.user });
-
-    if (isRedirect) router.replace("/");
+        if (isRedirect) {
+          router.replace("/");
+        }
+      },
+    });
   };
 };
 export default useLogout;
