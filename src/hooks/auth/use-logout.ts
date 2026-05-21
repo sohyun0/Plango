8e6d3ba3a8d000cc9e184b7af0b6d9595ef2f354
celine <wisohyun0@gmail.com>
2026-05-21 17:06:18 +0900

    fix: 로그아웃 기능 수정

--- src/hooks/auth/use-logout.ts
+++ src/hooks/auth/use-logout.ts
@@ -1,10 +1,15 @@
 import axiosInstance from "@/lib/axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+
 /**
  * 로그아웃 처리 훅, 필요시 메인 페이지 리다이렉트
  * @author sohyun
  */
+
+type LogoutOptions = {
+  isRedirect?: boolean;
+};
 
 const postLogout = async () => {
   const res = await axiosInstance.post("/auth/logout");
@@ -15,18 +20,23 @@ const useLogout = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
 
-  return async ({ isRedirect = true } = {}) => {
-    return useMutation({
-      mutationFn: postLogout,
-      onSettled: () => {
-        queryClient.clear();
-        router.refresh();
+  const logoutMutation = useMutation<unknown, Error, LogoutOptions>({
+    mutationFn: postLogout,
+    onSettled: (_data, _error, options) => {
+      const isRedirect = options?.isRedirect ?? true;
 
-        if (isRedirect) {
-          router.replace("/");
-        }
-      },
-    });
+      queryClient.clear();
+
+      if (isRedirect) {
+        router.replace("/");
+      }
+
+      router.refresh();
+    },
+  });
+
+  return (options: LogoutOptions = {}) => {
+    return logoutMutation.mutateAsync(options);
   };
 };
 export default useLogout;
